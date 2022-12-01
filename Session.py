1023483e898fe55669bc91aa2a5b5d8b8b34a86a
bartpia809 <bartpia809@student.polsl.pl>
2022-12-01 14:47:11 +0100

    Additional modes for drawing annotations

--- Session.py
+++ Session.py
@@ -2,22 +2,25 @@ from dataclasses import dataclass
 from IO import IO
 from UserInterface import UserInterface
 from ImageHandler import ImageHandler
-
+import numpy as np
+import cv2 
 
 class Session():
 
-    def _innit__(self):
-
-         __isPremium:bool
-         userInterface:UserInterface = UserInterface()
-         handler:ImageHandler = ImageHandler()
-         InOut:IO=IO()
+    def __init__(self):
+        self.__isPremium:bool
+        self.userInterface:UserInterface = UserInterface()
+        self.handler:ImageHandler = ImageHandler()
+        self.InOut:IO=IO()
 
 
     def __validateKey(self,key) -> bool:
         pass
 
 
+    def getUI(self)->UserInterface:
+        return self.userInterface
 
+imagePath="nowy.jpg"
 sesja = Session()
-
+sesja.getUI().openImageEditWindow(imagePath)
--- UserInterface.py
+++ UserInterface.py
@@ -1,7 +1,10 @@
+from ast import Str
 from dataclasses import dataclass
 import math
+from pickle import TRUE
     
 import numpy as np
+import os
 import cv2 
 @dataclass
 class UserInterface():
@@ -14,63 +17,86 @@ class UserInterface():
 
     def show():
         pass
+    
 
-img = cv2.imread("nowy.jpg")
-img2 = np.zeros((img.shape[0],img.shape[1],4), np.uint8)
-temp=img;
-drawing = False # true if mouse is pressed
-mode = True # if True, draw rectangle.
-ix,iy = -1,-1
+    def openImageEditWindow(self,imagePath:str):
+        if os.path.exists(imagePath)==False:
+            return
+            
+        inputImage = cv2.imread(imagePath)
+        tempimageName=os.path.basename(imagePath)
+        imageName= tempimageName[ :tempimageName.index(".")]
+        annotationLayer = np.zeros((inputImage.shape[0],inputImage.shape[1],4), np.uint8)
+        dummyImage=inputImage.copy()
 
-# mouse callback function
-def draw_circle(event,x,y,flags,param):
-  global ix,iy,drawing,mode
+        outputImage=imageName+"_annotated.jpg"
+        outputPath="./Annotations/"+outputImage
+        i=1
+        while(os.path.exists(outputPath)==True):#if exist add number to name
+            outputImage=imageName+"_annotated_"+str(i)+".jpg"
+            outputPath="./Annotations/"+outputImage
+            i+=1
 
-  if event == cv2.EVENT_LBUTTONDOWN:
-      drawing = True
-      ix,iy = x,y
+        drawing = False # true if mouse is pressed
+        handDrawMode = False # true if hand drawing mode is on
+        RectangleMode = True # if True, draw rectangle.
+        CircleMode = False # if True, draw circle.
+        ix,iy = -1,-1
+        # mouse callback function
+        def draw(event,x,y,flags,param):
+            global ix,iy
 
-  #elif event == cv2.EVENT_MOUSEMOVE:
-  #  if drawing == True:
-  #      if mode == True:
-  #          cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),3)
-  #          a=x
-  #          b=y
-  #          if a != x | b != y:
-  #               cv2.rectangle(img,(ix,iy),(x,y),(0,0,0),-1)
-  #      else:
-  #          cv2.circle(img,(x,y),5,(0,0,255),-1)
-  elif event == cv2.EVENT_LBUTTONUP:
-    drawing = False
-    if mode == True:
-        cv2.rectangle(img2,(ix,iy),(x,y),(0,255,0,255),2)
-        cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),2)
-
-    else:
-        sx=int((ix+x)/2)
-        sy=int((iy+y)/2)
-        truR=int(math.sqrt(math.pow(ix-x,2)+math.pow(iy-y,2))/2)
-        cv2.circle(img2,(sx,sy),truR,(0,0,255,255),2)
-        cv2.circle(img,(sx,sy),truR,(0,0,255,255),2)
+            if event == cv2.EVENT_LBUTTONDOWN:
+                drawing = True
+                ix,iy = x,y
+            #elif event== cv2.EVENT_MOUSEMOVE && handDrawMode==True:
+               # if drawing==True
 
 
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_circle)
+            elif event == cv2.EVENT_LBUTTONUP:
+                drawing = False
+                if RectangleMode == True:
+                    cv2.rectangle(annotationLayer,(ix,iy),(x,y),(0,255,0,255),2)
+                    cv2.rectangle(inputImage,(ix,iy),(x,y),(0,255,0),2)
+                elif CircleMode==True:
+                    sx=int((ix+x)/2)
+                    sy=int((iy+y)/2)
+                    truR=int(math.sqrt(math.pow(ix-x,2)+math.pow(iy-y,2))/2)
+                    cv2.circle(annotationLayer,(sx,sy),truR,(0,0,255,255),2)
+                    cv2.circle(inputImage,(sx,sy),truR,(0,0,255,255),2)
 
-while(1):
- cv2.imshow('image',img)
- #cv2.imshow('adnotation',img2)
- k = cv2.waitKey(1)
- print(k)
- if k == ord('m'):
-    mode = not mode
- elif k == ord('s'):
-    cv2.imwrite("./transparent_img.png",img2)
-    cv2.imwrite("./cat.png",img)
- elif k == 27:
-    break
+        cv2.namedWindow('image')
+        cv2.setMouseCallback('image',draw)
+        while(1):
+            cv2.imshow('image',inputImage)
+            #cv2.imshow('adnotation',img2)
+            k = cv2.waitKey(1)
+            if k == ord('r'):#Switch to draw rectangle
+               RectangleMode = True
+               CircleMode= False
+               handDrawMode = False
+            elif k == ord('e'):#Switch to draw circle
+               RectangleMode = False
+               CircleMode= True
+               handDrawMode = False
+            elif k == ord('d'):#Switch to hand draw
+               RectangleMode = False
+               CircleMode= False
+               handDrawMode=True
+            elif k == ord('s'):#saves edits in annotation
+               cv2.imwrite(outputPath,annotationLayer)
+               cv2.imwrite("./cat.png",inputImage)
+            elif k == ord('c'):#Clears with dummy image
+               inputImage=dummyImage.copy()
+               annotationLayer = np.zeros((inputImage.shape[0],inputImage.shape[1],4), np.uint8)
+            elif k == 27:#ESC
+               break
+            
+        cv2.destroyAllWindows()   
 
-cv2.destroyAllWindows()   
+        
+
+          
 
 
 
