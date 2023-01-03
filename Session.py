6865f463829070c2ecb9258a1597a423af677028
Bartek Piątek <quecheri@interia.pl>
2023-01-03 17:07:27 +0100

    dziwny marge

--- Session.py
+++ Session.py
@@ -14,9 +14,21 @@ class Session():
         self.handler:ImageHandler = ImageHandler()
         self.InOut:IO=IO()
         self.userInterface.groupBtn.clicked.connect(self.groupImages)
+        self.userInterface.browseBtn.clicked.connect(self.loadPaths)
 
     def __validateKey(self,key) -> bool:
         pass
+
+    def loadPaths(self):
+        self.userInterface.GetSource()
+        
+        #for fileName in glob.iglob(sourcePath + '**/**', recursive=True):
+        #    if os.path.isfile(fileName):
+        #        if fileName.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp')):
+        #            print(fileName)
+        #            self.tempList.append(fileName)
+
+        self.userInterface.SetupListView(self.InOut.readPath(self.userInterface.sourcePath))
 
 
     def getUI(self)->UserInterface.MainUserInterface:
@@ -25,7 +37,16 @@ class Session():
     def groupImages(self ):
         metryka:MetricI = Metrics.Object()
         self.handler.setMetric(metryka)
-        print(self.handler.group(self.userInterface.tempList))
+
+        dictonary = dict()
+        dictonary = self.handler.group(self.userInterface.contentList)
+        listP = list()
+
+        for key in dictonary.keys():
+            listP.append("########"+key+"########")
+            listP = listP + dictonary[key]
+
+        self.userInterface.SetupListView(listP)
         #self.userInterface.resutl = self.handler.group(self.userInterface.tempList)
 
 def main():
