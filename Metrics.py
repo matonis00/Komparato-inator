445f970ffbfdddc5a2004d42236fa6e80b391573
jakub22222 <qba6666@gmail.com>
2023-01-10 20:27:13 +0100

    color histogram method

--- Metrics.py
+++ Metrics.py
@@ -4,6 +4,13 @@ import os
 import glob
 import builtins
 
+#color histogram imports
+import imageio.v3 as iio
+import numpy as np
+import skimage.color
+import skimage.util
+import matplotlib.pyplot as plt
+
 
 @dataclass
 class MetricI():
@@ -13,7 +20,34 @@ class MetricI():
 
 @dataclass
 class ColorHistogram(MetricI):
-    pass
+    def __init__(self):
+        
+        print("Utworzono metryke histogramu kolorow")
+
+    def drawHistogram(self):
+        image = iio.imread(uri="img\dog2.jpg")
+        
+        # display the image
+        fig, ax = plt.subplots()
+        plt.imshow(image)
+        # tuple to select colors of each channel line
+        colors = ("red", "green", "blue")
+        
+        # create the histogram plot, with three lines, one for
+        # each color
+        plt.figure()
+        plt.xlim([0, 256])
+        for channel_id, color in enumerate(colors):
+            histogram, bin_edges = np.histogram(image[:, :, channel_id], bins=256, range=(0, 256))
+            plt.plot(bin_edges[0:-1], histogram, color=color)
+            
+        
+        plt.title("Color Histogram")
+        plt.xlabel("Color value")
+        plt.ylabel("Pixel count")
+        
+        plt.show()
+   
 
 
 @dataclass
