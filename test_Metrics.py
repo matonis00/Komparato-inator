17f169c46c077ab2c4ff5594cae82e9a555d7e92
mategor195 <mategor195@student.polsl.pl>
2023-01-21 13:04:06 +0100

    paths update in tests

--- test_Metrics.py
+++ test_Metrics.py
@@ -9,35 +9,35 @@ class Test_test_Metrics(unittest.TestCase):
 
         imageList = list()
 
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\airplane1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\airplane2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\bananas.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\bicycleAndCar.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\bicycleAndPerson.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\bird1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\bird2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\boat1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\boat2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\bus1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\bus2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\car1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\car2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\cow1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\cow2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\dog1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\dog2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\horse1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\horse2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\motorcycle1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\motorcycle2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\personAndDog.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\personAndTie.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\sheep1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\sheep2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\train1.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\train2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\truck2.jpg');
-        imageList.append('D:\\Projekty\\IO\\PROJEKT\\komparato-inator\\img\\truck2.jpg');
+        imageList.append(str(os.path.abspath('img\\airplane1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\airplane2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\bananas.jpg')));
+        imageList.append(str(os.path.abspath('\img\\bicycleAndCar.jpg')));
+        imageList.append(str(os.path.abspath('\img\\bicycleAndPerson.jpg')));
+        imageList.append(str(os.path.abspath('\img\\bird1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\bird2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\boat1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\boat2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\bus1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\bus2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\car1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\car2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\cow1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\cow2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\dog1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\dog2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\horse1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\horse2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\motorcycle1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\motorcycle2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\personAndDog.jpg')));
+        imageList.append(str(os.path.abspath('\img\\personAndTie.jpg')));
+        imageList.append(str(os.path.abspath('\img\\sheep1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\sheep2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\train1.jpg')));
+        imageList.append(str(os.path.abspath('\img\\train2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\truck2.jpg')));
+        imageList.append(str(os.path.abspath('\img\\truck2.jpg')));
 
         result = obj.group(imageList)
 
@@ -81,26 +81,23 @@ class Test_test_Metrics(unittest.TestCase):
 
         imageList = list()
 
-        imageList.append('C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\cat1.jpg');
-        imageList.append('C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\cat2.png');
-        imageList.append('C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\cat3.jpg');
-
-        imageList.append('C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\cow1.jpg');
-
-        imageList.append('C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\dog1.jpg');
-        imageList.append('C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\dog2.jpg');
-
-        imageList.append('C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\lcd1.jpg');
+        imageList.append(str(os.path.abspath('similar\\cat1.jpg')));
+        imageList.append(str(os.path.abspath('similar\\cat2.png')));
+        imageList.append(str(os.path.abspath('similar\\cat3.jpg')));
+        imageList.append(str(os.path.abspath('similar\\cow1.jpg')));
+        imageList.append(str(os.path.abspath('similar\\dog1.jpg')));
+        imageList.append(str(os.path.abspath('similar\\dog2.jpg')));
+        imageList.append(str(os.path.abspath('similar\\lcd1.jpg')));
 
         result = obj.group(imageList)
 
-        catSet = {"C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\cat1.jpg", 
-                  "C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\cat2.png", 
-                  "C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\cat3.jpg",
-                  'C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\dog2.jpg'}
+        catSet = {str(os.path.abspath('similar\\cat1.jpg')), 
+                  str(os.path.abspath('similar\\cat2.png')), 
+                  str(os.path.abspath('similar\\cat3.jpg')),
+                  str(os.path.abspath('similar\\dog2.jpg'))}
 
-        dogSet = {'C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\dog1.jpg',
-                  'C:\\Users\\Jakub\\Desktop\\IO\\komparato-inator\\similar\\dog2.jpg'}
+        dogSet = {str(os.path.abspath('similar\\dog1.jpg')),
+                  str(os.path.abspath('similar\\dog2.jpg'))}
 
         foundGroups = 0
         for key in result:
